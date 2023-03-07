3f79dadec875ee3a2b249f0b6fdc60b5f43b2c04
HyeonU-Jo <chw3448@gmail.com>
2023-03-07 19:42:25 +0900

    Add: tailwind 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,7 @@
-import React from "react";
-
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-
-import {
-    RecoilRoot,
-    atom,
-    selector,
-    useRecoilState,
-    useRecoilValue,
-} from "recoil";
-
-const GlobalStyles = createGlobalStyle`
-    ${reset}
-    /* other styles */
-`;
+import React from 'react';
 
 function App() {
-    return (
-        <RecoilRoot>
-            <GlobalStyles />
-            <div>Hello, world!</div>
-        </RecoilRoot>
-    );
+    return <></>;
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './assets/tailwind.css';
+
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+import { RecoilRoot } from 'recoil';
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    /* other styles */
+`;
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <RecoilRoot>
+            <GlobalStyles />
+            <App />
+        </RecoilRoot>
     </React.StrictMode>
 );
