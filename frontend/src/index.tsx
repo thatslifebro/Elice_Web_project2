bdafdd32dce764842ce8483d7ee9e689b48b6edf
HyeonU-Jo <chw3448@gmail.com>
2023-03-08 14:14:13 +0900

    Chore: tailwind 경로 수정 및 temp 파일 삭제

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './assets/tailwind.css';
+import './styles/tailwind.css';
 
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
