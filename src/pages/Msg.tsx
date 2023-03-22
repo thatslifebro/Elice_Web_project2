dc3d0f67f0b61ddd993174c3859cecb155c632d4
jingwangjjang <jingwangjjang@gmail.com>
2023-03-23 06:02:00 +0900

    feat: 쪽지 보기 가능, 정해진 상대에게 보내는 기능 구현 필요

--- src/pages/Msg.tsx
+++ src/pages/Msg.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { AxiosError } from 'axios';
-import { customAxios } from '../utils/customAxios';
+import { api } from '../utils/customAxios';
 
 interface FormValue {
     content: string;
-    // receiverId: number;
+    receiverId: number;
 }
 
 function Msg() {
@@ -14,21 +13,61 @@ function Msg() {
         handleSubmit,
         formState: { errors },
     } = useForm<FormValue>();
+    
+    const [Msg, setMsg] = useState([]);
+    const [Update, setUpdate] = useState(0);
+    let i = 1;
+    const user = JSON.parse(localStorage.getItem('user') || 'Anonymous');
+
+    const getMsg = async ()=>{
+        try {
+            const Inmsg = await api.get(`api/users/${user.id}/letters/inbox?page=1&limit=30`);
+            const Outmsg = await api.get(`api/users/${user.id}/letters/outbox?page=1&limit=30`);
+            let res = [];
+            if (Inmsg.data.data) {
+                res = Inmsg.data.data.concat(Outmsg.data.data);
+            }
+            else if (Outmsg.data.data) {
+                res = Outmsg.data.data.concat(Inmsg.data.data);
+            }
+            res = res.filter((element: Object) => element !== undefined);
+            setMsg(res);
+        } catch (error : any) {
+            alert("Invalid request");
+        }
+    };
+
+    const deleteInbox = async (userId: number, id: number)=>{
+        try {
+            const response = await api.delete(`api/users/${userId}/letters/${id}/inbox`);
+            setUpdate(i);
+            i = i + 1;
+        } catch (error : any) {
+            alert("Invalid request");
+        }
+    };
+
+    const deleteOutbox = async (userId: number, id: number)=>{
+        try {
+            const response = await api.delete(`api/users/${userId}/letters/${id}/outbox`);
+            setUpdate(i);
+            i = i + 1;
+        } catch (error : any) {
+            alert("Invalid request");
+        }
+    };
+
+    useEffect(()=>{
+        getMsg();
+    }, [Update]);
 
     const onSubmitHandler: SubmitHandler<FormValue> = async (data) => {
         try {
-            customAxios
-            .post('/api/letters', data)
-            .then((response) => response.data)
-            .then((data) => {
-                if (data.data === 'OK') {
-                    alert('정상적으로 가입되었습니다.');
-                }
-            })
-        } catch (error) {
-            console.error(error);
-            if (error instanceof AxiosError)
-                alert(error.response?.data.errorMessage);
+            data.receiverId = 30;
+            const response = await api.post(`/api/users/${user.id}/letters`, data);
+            window.location.replace("/msg")
+        } catch (error : any) {
+            alert("Invalid request");
         }
     };
 
@@ -48,20 +87,26 @@ function Msg() {
 
                 <div className='flex flex-col w-1/2 h-full p-3 border-solid border border-gray-300 rounded-md'>
                     <div className='text-xl font-bold m-2 border-solid border-b border-black'>닉네임</div>
-                    <div className='m-3'>
-                        <div className='flex place-content-between'>
-                            <div className='text-xs font-semibold text-yellow-500'>보낸 쪽지</div>
-                            <div className='text-xs'>23/09/03 18:05</div>
-                        </div>
-                        <div className='text-base truncate'>안녕하세요~ ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</div>
-                    </div>
-                    <div className='m-3'>
-                        <div className='flex place-content-between'>
-                            <div className='text-xs font-semibold text-green-500'>받은 쪽지</div>
-                            <div className='text-xs'>23/09/03 18:05</div>
-                        </div>
-                        <div className='text-base truncate'>반갑습니다~ ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</div>
-                    </div>
+                        {Msg &&
+                            Msg.map((msg: {content: string, id: number, authorId: number, receiverId: number})=>{
+                                console.log(Msg)
+                                return(
+                                    <div className='m-3'>
+                                        <div className='flex place-content-between'>
+                                            {msg.receiverId === user.id ?
+                                                <div className='text-xs font-semibold text-yellow-500'>받은 쪽지</div> :
+                                                <div className='text-xs font-semibold text-green-500'>보낸 쪽지</div>
+                                            }
+                                            {msg.receiverId === user.id ?
+                                                <button onClick={() => deleteInbox(msg.receiverId, msg.id)}className='text-xs text-red-700'>받은 삭제</button> :
+                                                <button onClick={() => deleteOutbox(msg.authorId, msg.id)}className='text-xs text-red-700'>보낸 삭제</button>
+                                            }
+                                        </div>
+                                        <div className='text-base truncate'>{msg.content}</div>
+                                    </div>                             
+                                )
+                            })
+                        }
 
                     <form onSubmit={handleSubmit(onSubmitHandler)} className='w-full'>
                         <input 
@@ -72,12 +117,12 @@ function Msg() {
                             })}
                             className='h-16 w-5/6 m-3 border-solid border border-gray-300 rounded-md'
                         />
-                        <button className=''>보내기</button>
+                        <button>보내기</button>
                     </form>
-                    {errors.content?.type === 'minLength' &&
+                    {/* {errors.content?.type === 'minLength' &&
                     '최소 1자부터 입력할 수 있습니다!'}
                     {errors.content?.type === 'maxLength' &&
-                    '최대 200자까지 입력할 수 있습니다!'}
+                    '최대 200자까지 입력할 수 있습니다!'} */}
                 </div>
             </div>
         </>
