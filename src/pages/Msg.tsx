82ff1a876f58077778431b79a9a7c5f6ceef9c95
jingwangjjang <jingwangjjang@gmail.com>
2023-03-23 22:14:52 +0900

    feat: 쪽지 보내기/받기 기능 완성

--- src/pages/Msg.tsx
+++ src/pages/Msg.tsx
@@ -7,6 +7,15 @@ interface FormValue {
     receiverId: number;
 }
 
+// interface message {
+//     id: number,
+//     authorId: number,
+//     authorName: string,
+//     receiverId: number,
+//     content: string,
+//     createdAt: Date
+// }
+
 function Msg() {
     const {
         register,
@@ -15,14 +24,16 @@ function Msg() {
     } = useForm<FormValue>();
     
     const [Msg, setMsg] = useState([]);
-    const [Update, setUpdate] = useState(0);
-    let i = 1;
+    const [Groupby, setGroupby] = useState([]);
+    const [Receiver, setReceiver] = useState(0);
     const user = JSON.parse(localStorage.getItem('user') || 'Anonymous');
 
     const getMsg = async ()=>{
         try {
             const Inmsg = await api.get(`api/users/${user.id}/letters/inbox?page=1&limit=30`);
             const Outmsg = await api.get(`api/users/${user.id}/letters/outbox?page=1&limit=30`);
+
+            // 보낸, 받은 쪽지 데이터 통합
             let res = [];
             if (Inmsg.data.data) {
                 res = Inmsg.data.data.concat(Outmsg.data.data);
@@ -30,45 +41,88 @@ function Msg() {
             else if (Outmsg.data.data) {
                 res = Outmsg.data.data.concat(Inmsg.data.data);
             }
+
+            // 보낸쪽지나 받은쪽지가 하나도 없을때 undefined 값 제거
             res = res.filter((element: Object) => element !== undefined);
-            setMsg(res);
+
+            // 날짜 기준 오름차순 정렬
+            res = res.sort(function(a: any, b: any) {
+                return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+            });
+            
+            // 대화중인 상대방을 알기 위해 nickname 키 값 추가
+            for (let element of res) {
+                if(Object.keys(element).includes('authorName')) {
+                    element.nickname = element.authorName;
+                } else {
+                    element.nickname = element.receiverName;
+                }
+            }
+            
+            // nickname별로 group
+            let gb = res.reduce((x: any, y: any) => {
+                (x[y.nickname] = x[y.nickname] || []).push(y);
+                return x;
+            }, {});
+
+            setGroupby(gb);
         } catch (error : any) {
-            alert("Invalid request");
+            alert(error);
         }
     };
 
     const deleteInbox = async (userId: number, id: number)=>{
         try {
-            const response = await api.delete(`api/users/${userId}/letters/${id}/inbox`);
-            setUpdate(i);
-            i = i + 1;
+            await api.delete(`api/users/${userId}/letters/${id}/inbox`);
+            window.location.replace("/msg")
         } catch (error : any) {
-            alert("Invalid request");
+            alert(error);
         }
     };
 
     const deleteOutbox = async (userId: number, id: number)=>{
         try {
-            const response = await api.delete(`api/users/${userId}/letters/${id}/outbox`);
-            setUpdate(i);
-            i = i + 1;
+            await api.delete(`api/users/${userId}/letters/${id}/outbox`);
+            window.location.replace("/msg")
         } catch (error : any) {
-            alert("Invalid request");
+            alert(error);
         }
     };
 
     useEffect(()=>{
         getMsg();
-    }, [Update]);
+    }, []);
 
+    // 쪽지를 보내는 함수
     const onSubmitHandler: SubmitHandler<FormValue> = async (data) => {
         try {
-            data.receiverId = 30;
-            const response = await api.post(`/api/users/${user.id}/letters`, data);
+            data.receiverId = Receiver;
+            await api.post(`/api/users/${user.id}/letters`, data);
             window.location.replace("/msg")
         } catch (error : any) {
             alert("Invalid request");
         }
+    };
+
+    // 쪽지함을 보여주는 함수
+    const showMsgBox = () => {
+        const result = [];
+
+        for (let k in Groupby) {
+            const v: any = Groupby[k]
+            result.push(
+                <button onClick={() => {
+                getMsg();
+                setMsg(v);
+                setReceiver(v[0].authorId !== user.id ? v[0].authorId : v[0].receiverId);
+                }} 
+                className='m-3 hover:bg-blue-200'>
+                    <div className='text-base'>{k}님과의 쪽지</div>
+                </button>                
+            );
+        }
+
+        return result;
     };
 
     return (
@@ -76,30 +130,27 @@ function Msg() {
             <div className='flex items-center justify-center p-5' style={{height: '51.7rem'}}>
                 <div className='flex flex-col w-1/5 h-full p-3 mx-2 border-solid border border-gray-300 rounded-md'>
                     <div className='text-xl font-bold m-2 border-solid border-b border-black'>쪽지함</div>
-                    <div className='m-3'>
-                        <div className='flex place-content-between'>
-                            <div className='text-xs font-semibold'>익명</div>
-                            <div className='text-xs'>23/09/03 18:05</div>
-                        </div>
-                        <div className='text-base truncate'>안녕하세요~ ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</div>
-                    </div>
+                    {Groupby && showMsgBox()}
                 </div>
 
-                <div className='flex flex-col w-1/2 h-full p-3 border-solid border border-gray-300 rounded-md'>
-                    <div className='text-xl font-bold m-2 border-solid border-b border-black'>닉네임</div>
+                <div className='flex flex-col w-1/2 h-full p-3 border-solid border border-gray-300 rounded-md overflow-auto'>
+                    <div className='text-xl font-bold m-2 border-solid border-b border-black'>내역</div>
                         {Msg &&
-                            Msg.map((msg: {content: string, id: number, authorId: number, receiverId: number})=>{
-                                console.log(Msg)
+                            Msg.map((msg: {content: string, id: number, authorId: number, receiverId: number, createdAt: string})=>{
+                                // 쪽지 생성시간 한국 시간대로 변경
+                                const date = new Date(msg.createdAt);
+                                const date_kor = date.toString();
+
                                 return(
                                     <div className='m-3'>
                                         <div className='flex place-content-between'>
                                             {msg.receiverId === user.id ?
-                                                <div className='text-xs font-semibold text-yellow-500'>받은 쪽지</div> :
-                                                <div className='text-xs font-semibold text-green-500'>보낸 쪽지</div>
+                                                <div className='text-xs font-semibold text-yellow-500'>받은 쪽지 | {date_kor}</div> :
+                                                <div className='text-xs font-semibold text-green-500'>보낸 쪽지 | {date_kor}</div>
                                             }
                                             {msg.receiverId === user.id ?
-                                                <button onClick={() => deleteInbox(msg.receiverId, msg.id)}className='text-xs text-red-700'>받은 삭제</button> :
-                                                <button onClick={() => deleteOutbox(msg.authorId, msg.id)}className='text-xs text-red-700'>보낸 삭제</button>
+                                                <button onClick={() => deleteInbox(msg.receiverId, msg.id)}className='text-xs text-red-700'>삭제</button> :
+                                                <button onClick={() => deleteOutbox(msg.authorId, msg.id)}className='text-xs text-red-700'>삭제</button>
                                             }
                                         </div>
                                         <div className='text-base truncate'>{msg.content}</div>
