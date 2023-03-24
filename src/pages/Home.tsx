d9e63b0e437e74efab17b10f81b76ebc40f85e5a
HyeonU-Jo <chw3448@gmail.com>
2023-03-24 15:30:20 +0900

    style: favicon 및 title 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
             <TypeIt
                 className='absolute top-[15%] left-[5%] text-6xl'
                 options={{
-                    strings: ['TIME TO NEW WAVE'],
+                    strings: ['우리... 같이 운동할까요?'],
                     speed: 100,
                     waitUntilVisible: true,
                 }}
